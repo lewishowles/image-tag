83df96f97dd0f0171a30a5dfdaae98a7c39b5792
Lewis Howles <lewishowles@users.noreply.github.com>
2025-07-28 14:45:48 +0100

    Add "fallback" slot to allow custom fallback designs

--- image-tag.js
+++ image-tag.js
@@ -189,16 +189,18 @@ class ImageTag extends HTMLElement {
 		this.fallbackElementTemplate = document.createElement('template');
 
 		this.fallbackElementTemplate.innerHTML = `
-			<div class="image-tag-fallback">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" role="presentation" class="image-tag-fallback-icon">
-					<g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
-						<path stroke="currentColor" d="m6.837 12.493 5.206-5.2a1 1 0 0 1 1.414 0l3.043 3.043" />
-						<path stroke="currentColor" d="M2 7v6.5a2 2 0 0 0 2 2h8.5" />
-						<rect width="11.5" height="9.5" x="5" y="3" stroke="currentColor" rx="2" transform="rotate(180 10.75 7.75)" />
-						<path fill="currentColor" fill-rule="nonzero" d="M8.25 7.25c-.551 0-1-.449-1-1s.449-1 1-1 1 .449 1 1-.449 1-1 1" />
-					</g>
-				</svg>
-			</div>
+			<slot name="fallback">
+				<div class="image-tag-fallback">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" role="presentation" class="image-tag-fallback-icon">
+						<g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+							<path stroke="currentColor" d="m6.837 12.493 5.206-5.2a1 1 0 0 1 1.414 0l3.043 3.043" />
+							<path stroke="currentColor" d="M2 7v6.5a2 2 0 0 0 2 2h8.5" />
+							<rect width="11.5" height="9.5" x="5" y="3" stroke="currentColor" rx="2" transform="rotate(180 10.75 7.75)" />
+							<path fill="currentColor" fill-rule="nonzero" d="M8.25 7.25c-.551 0-1-.449-1-1s.449-1 1-1 1 .449 1 1-.449 1-1 1" />
+						</g>
+					</svg>
+				</div>
+			</slot>
 		`;
 	}
 
